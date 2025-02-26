b870e61ac71740ad5eb3cf3d400ab3a3090c639b
Zoedube <144275796+Zoedube@users.noreply.github.com>
2025-02-26 16:52:19 +0200

    Update new deployment CORS

--- api/index.js
+++ api/index.js
@@ -31,6 +31,7 @@ app.use(cors({
 // ✅ Fix CORS issue
 const allowedOrigins = [
   "https://blog-rjbl.vercel.app", // ✅ Your Vercel frontend URL
+  "blog-rjbl-7dj14ydv5-zoedubes-projects.vercel.app",
   "http://localhost:5173" // ✅ For local development (optional)
 ];
 
