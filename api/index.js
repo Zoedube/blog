c43661bc26f0c626a9c76803fab3ceb57d8fea15
Zoedube <144275796+Zoedube@users.noreply.github.com>
2025-02-26 16:23:08 +0200

    Update and fix the CORS

--- api/index.js
+++ api/index.js
@@ -26,6 +26,8 @@ app.use(cors({
   credentials: true  
 }));
 
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
